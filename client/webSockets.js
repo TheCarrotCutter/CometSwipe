65286fd6c819873a3e9153f71dacdb485e808501
TheCarrotCutter <masonthedecker@gmail.com>
2025-08-14 09:28:19 -0400

    Delete client directory

--- client/webSockets.js
+++
@@ -1,6 +0,0 @@
-const ws = new webSocket('https://cometswipe-test.onrender.com');
-
-ws.onmessage = (event) => {
-  const output = document.getElementById('output');
-  output.textContent += 'Server: ' + event.data + '\n';
-};

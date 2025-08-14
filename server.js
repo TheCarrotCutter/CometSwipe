570a70c21bfc3a3650439c41eb6142d24572c03b
TheCarrotCutter <masonthedecker@gmail.com>
2025-08-14 10:01:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000; // Render will set process.env.PORT
 const wss = new WebSocket.Server({ port: PORT });
+
+wss.on('connection', ws => {
+  console.log('Client connected');
+});

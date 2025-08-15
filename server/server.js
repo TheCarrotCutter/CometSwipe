cb4b4c4ad376cb1b36d8f698df358d0086a968f2
TheCarrotCutter <masonthedecker@gmail.com>
2025-08-15 09:47:00 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const wss = new WebSocket.Server({ port: PORT });
 
 wss.on('connection', ws => {
   console.log('Client connected');
-});
 
-WebSocket.onmessage = function(event) {
-  console.log(event.data);
-  WebSocket.send(event.data);
-};
+    ws.on('message', function incoming(message) {
+    console.log(message);
+    ws.send(message); // Echoes the received message back to the client
+  });
+});
